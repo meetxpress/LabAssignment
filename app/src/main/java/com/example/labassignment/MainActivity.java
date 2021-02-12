fcfb485e2fd49c05672baf0f1ba64853687f7c44
meetxpress <pmeet7895@gmail.com>
2021-02-12 20:11:17 +0530

    Kept only Assignment 1.

--- app/src/main/java/com/example/labassignment/MainActivity.java
+++ app/src/main/java/com/example/labassignment/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,17 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setDisplayShowCustomEnabled(true);
     }
 
-    public void gotoMainActivty(View v) {
+    public void gotoActivity_ass1(View v) {
         switch (v.getId()) {
-            case R.id.lab1:
-                startActivity(new Intent(getApplicationContext(), assignment1.class));
+            case R.id.btnque1:
+                startActivity(new Intent(getApplicationContext(), Ass1_Q1.class));
                 break;
 
-            case R.id.lab2:
-                startActivity(new Intent(getApplicationContext(), assignment2.class));
+            case R.id.btnque2:
+                startActivity(new Intent(getApplicationContext(), Ass1_Q2.class));
                 break;
+
+            case R.id.btnque3:
+                startActivity(new Intent(getApplicationContext(), Ass1_Q3.class));
+                break;
+
+            default:
         }
     }
 }
